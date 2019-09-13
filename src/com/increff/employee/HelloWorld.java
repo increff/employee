9f1ca2b582e06082e572bea825a72db19b833f94
unknown <romil.jain@nextscm.com>
2019-09-13 15:58:06 +0530

    draft code

---
+++ src/com/increff/employee/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.increff.employee;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorld2.main(args);
+	}
+
+}
---
+++ src/com/increff/employee/HelloWorld3.java
@@ -0,0 +1,9 @@
+package com.increff.employee;
+
+public class HelloWorld3 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}

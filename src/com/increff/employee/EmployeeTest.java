7150fa64c58fdaab9bdbe04af0dd9a843a739978
unknown <romil.jain@nextscm.com>
2019-09-16 10:19:02 +0530

    added properties logging and junit

---
+++ src/com/increff/employee/EmployeeTest.java
@@ -0,0 +1,25 @@
+package com.increff.employee;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.ResultSet;
+
+import org.junit.Test;
+
+public class EmployeeTest {
+
+	@Test
+	public void sayHello() throws Exception {
+		EmployeeApi api = new EmployeeApi();
+		api.delete();
+		api.insert();
+		ResultSet rs = api.select();
+		int i = 0;
+		while (rs.next()) {
+			i++;
+		}
+		assertEquals(3, i);
+
+	}
+
+}
